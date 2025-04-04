725cceb1ed65678a30ffe8fa159374dd71f9da3c
Sonu Rai <sonukumar9905757@gmail.com>
2025-04-04 20:38:35 +0530

    first-commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({subsets: ["latin"]});
+
+export const metadata: Metadata = {
+  title: "Portfolio - Sonu Rai",
+  description: "A portfolio website of SonuRai",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
